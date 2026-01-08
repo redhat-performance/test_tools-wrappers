7d378d5137690c1197f2ebd3fdb3de3b4d335213
Keith Valin <kvalin@redhat.com>
2026-01-08 15:39:25 -0500

    ci(detect_os): Add tests for detect_os --os-version commands

--- tests/test_detect_os.py
+++ tests/test_detect_os.py
@@ -7,7 +7,7 @@ from .util import run_script
 
 DETECT_OS_SCRIPT="./detect_os"
 
-def create_os_release_file(os_name: str, os_version: str, dest: Path):
+def create_os_release_file(dest: Path, os_name: str = "fedora", os_version: str = "43"):
     dest.write_text(
     f"""
         NAME="{os_name} Linux"
@@ -31,7 +31,53 @@ def test_detect_os_help(flag):
 @pytest.mark.parametrize("name", ["sles", "rhel", "fedora", "ubuntu", "amzn"])
 def test_detect_os_name(name: str, tmp_path):
     d = tmp_path / f"os-release"
-    create_os_release_file(name, "42", d)
+    create_os_release_file(d, name)
     stdout, stderr, rtc = run_script([DETECT_OS_SCRIPT, "--os-release-file", d])
     assert rtc == 0
     assert stdout.decode('utf-8').strip() == name
+
+@pytest.mark.parametrize("version", ["43", "22.04", "24.04", "9.6", "2023", "15.7"])
+def test_detect_os_version(version: str, tmp_path):
+    d = tmp_path / f"os-release"
+    create_os_release_file(d, os_version=version)
+    stdout, stderr, rtc = run_script([
+        DETECT_OS_SCRIPT,
+        "--os-release-file",
+        d,
+        "--os-version"
+    ])
+
+    assert rtc == 0
+    assert stdout.decode('utf-8').strip() == version
+
+@pytest.mark.parametrize("version", ["22.04", "24.04", "9.6", "15.7"])
+def test_detect_os_major_version(version: str, tmp_path):
+    d = tmp_path / f"os-release"
+    create_os_release_file(d, os_version=version)
+    stdout, stderr, rtc = run_script([
+        DETECT_OS_SCRIPT,
+        "--os-release-file",
+        d,
+        "--os-version",
+        "--major-version"
+    ])
+
+    assert rtc == 0
+    assert stdout.decode('utf-8').strip() == version.split('.')[0]
+
+@pytest.mark.parametrize("version", ["22.04", "24.04", "9.6", "15.7"])
+def test_detect_os_minor_version(version: str, tmp_path):
+    d = tmp_path / f"os-release"
+    create_os_release_file(d, os_version=version)
+    stdout, stderr, rtc = run_script([
+        DETECT_OS_SCRIPT,
+        "--os-release-file",
+        d,
+        "--os-version",
+        "--minor-version"
+    ])
+
+    assert rtc == 0
+    assert stdout.decode('utf-8').strip() == version.split('.')[1]
+
+
