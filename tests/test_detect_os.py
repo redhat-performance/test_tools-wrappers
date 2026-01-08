a3574e3a037090175cbb5e06735bcc6557de9efe
Keith Valin <kvalin@redhat.com>
2026-01-08 13:30:29 -0500

    ci(detect_os): Add tests for detect_os returning OS names

--- tests/test_detect_os.py
+++ tests/test_detect_os.py
@@ -1,8 +1,22 @@
 import pytest
+import tempfile
+
+from pathlib import Path
 
 from .util import run_script
 
 DETECT_OS_SCRIPT="./detect_os"
+
+def create_os_release_file(os_name: str, os_version: str, dest: Path):
+    dest.write_text(
+    f"""
+        NAME="{os_name} Linux"
+        VERSION_ID={os_version}
+        ID={os_name}
+        VERSION_CODE_NAME=""
+    """
+    )
+    
 
 def test_detect_os_invalid_opts():
     stdout, stderr, rtc = run_script([DETECT_OS_SCRIPT, "--invalid"])
@@ -13,3 +27,11 @@ def test_detect_os_help(flag):
     stdout, stderr, rtc = run_script([DETECT_OS_SCRIPT, flag])
     print(rtc)
     assert rtc == 0
+
+@pytest.mark.parametrize("name", ["sles", "rhel", "fedora", "ubuntu", "amzn"])
+def test_detect_os_name(name: str, tmp_path):
+    d = tmp_path / f"os-release"
+    create_os_release_file(name, "42", d)
+    stdout, stderr, rtc = run_script([DETECT_OS_SCRIPT, "--os-release-file", d])
+    assert rtc == 0
+    assert stdout.decode('utf-8').strip() == name
