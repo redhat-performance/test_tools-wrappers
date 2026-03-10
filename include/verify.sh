971058f3be7ecd785990086ae4e0ed8185cb2250
Keith Valin <kvalin@redhat.com>
2026-03-10 15:45:11 -0400

    fix(verify): Bump SLES's python version
    Bumps SLES's python version to python3.11 instead of the default 3.6,
    which is too old for pydantic 2.0.0+.

--- include/verify.sh
+++ include/verify.sh
@@ -2,16 +2,29 @@
 
 setup_verification() {
     os=$("$TOOLS_BIN"/detect_os)
+    install_flags=""
 
     # Ubuntu started shipping with their python3-typing extensions
     # which is too old for pydantic and should be installed via pip so it needs to be removed
     if [[ "$os" == "ubuntu" ]]; then
         #Don't care if this fails, since then it means that it wasn't installed to begin with
         package_tool --remove_packages python3-typing-extensions &> /dev/null
+    elif [[ "$os" == "sles" ]]; then
+        # SLES's default python interpreter is python 3.6, which is too old for
+        # pydantic 2.0.0+, so we need a newer intepreter
+        install_flags="--python_exec python3.11"
     fi
-	package_tool --wrapper_config ${TOOLS_BIN}/deps/verification.json
+
+	package_tool --wrapper_config ${TOOLS_BIN}/deps/verification.json $install_flags
 }
 
 verify_results() {
-    "$TOOLS_BIN/verify_results" $to_verify_flags "$@"
+    cmd="python3"
+    os=$("$TOOLS_BIN"/detect_os)
+    
+    if [[ "$os" == "sles" ]]; then
+        cmd="python3.11"
+    fi
+
+    $cmd "$TOOLS_BIN/verify_results" $to_verify_flags "$@"
 }
