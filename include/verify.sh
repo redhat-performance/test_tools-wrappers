3099650a7206b1690d7b0ea43064a25391ed174b
Keith Valin <kvalin@redhat.com>
2026-03-10 15:43:31 -0400

    refactor: remove typing-extensions on Ubuntu only

--- include/verify.sh
+++ include/verify.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
 setup_verification() {
-	# Ubuntu started shipping with their python3-typing extensions
-	# which is too old for pydantic and should be installed via pip so it needs to be removed
-	package_tool --remove_packages python3-typing-extensions
+    os=$("$TOOLS_BIN"/detect_os)
+
+    # Ubuntu started shipping with their python3-typing extensions
+    # which is too old for pydantic and should be installed via pip so it needs to be removed
+    if [[ "$os" == "ubuntu" ]]; then
+        #Don't care if this fails, since then it means that it wasn't installed to begin with
+        package_tool --remove_packages python3-typing-extensions &> /dev/null
+    fi
 	package_tool --wrapper_config ${TOOLS_BIN}/deps/verification.json
 }
 
